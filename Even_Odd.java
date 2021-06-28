c0e60e8ccaa8dcb82be3272468740bdd6a36b313
aniketratnaparakhi <55104024+aniketratnaparakhi@users.noreply.github.com>
2021-06-28 00:53:20 -1200

    Update Even_Odd.java

--- Even_Odd.java
+++ Even_Odd.java
@@ -4,27 +4,7 @@ public class Even_Odd {
 
 	public static void main(String[] args)
 	{
-		int n,k,j;
-		int i;
-		k=0;
-		j=0;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter The Number: ");
-		n = sc.nextInt();
-		for (i=1; i<=n; i++)
-		{
-			if (i%2==0)
-			{
-				System.out.println(i+ "  is Even");
-				k=k+1;
-			}
-			else
-			{
-				System.out.println(i+ "  is Odd");
-				j=j+1;
-			}
-		}
-		System.out.println("Totle Odd Number is = " + j +"\nTotle Even Number is = "+k);
+		System.out.println("Hello World");
 	}
 
 }
